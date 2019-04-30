77fa20758a83a021d22575cdfe34d21b40db5340
Umar--Khan <umarkhandev@gmail.com>
2019-04-30 15:45:47 +0100

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const div = document.querySelector('#show-panel')
 const title = document.querySelector('#list-panel')
 
 const h3 = document.createElement('h3')
-h3.innerText = 'Double Click to View a Book'
+h3.innerText = 'Click to View a Book'
 title.prepend(h3)
 
 const back = document.createElement('button')
 back.innerText = 'Go Back'
 title.append(back)
 
-back.addEventListener('click', () =>{
+back.addEventListener('click', () => {
     div.innerHTML = ""
 })
+
+const currentUser = {
+    "id": 1,
+    "username": "pouros"
+}
 
 //! API
 const getBooks = () =>
@@ -28,7 +33,9 @@ const updateBook = book =>
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ users: book.users })
+        body: JSON.stringify({
+            users: book.users
+        })
     }).then(resp => resp.json())
 
 //! Code
@@ -37,45 +44,56 @@ const renderBook = book => {
     li.innerText = `${book.title}`
     list.append(li)
 
-    li.addEventListener('dblclick', () => {
+    li.addEventListener('click', () => {
         refresh(book)
+        checkUser(book)
 
         const read = div.querySelector('#read')
         const unlike = div.querySelector('#unlike')
 
-        read.addEventListener('click', () =>{
-            const check = book.users.find(user=>user.id == 1)
-        if(check){
-            alert('You Already Read This Book')
-        } else{
-            book.users.push({"id":1, "username":"You"})
-            updateBook(book)
-            refresh(book)
-        }})
+        read.addEventListener('click', () => {
+            const check = book.users.find(user => user.id == currentUser.id)
+            if (check) {
+                alert('You Already Read This Book')
+            } else {
+                checkUser(book)
+            }
+        })
 
-        unlike.addEventListener('click', () =>{
-            const checking = book.users.find(user=>user.id == 1)
-        if(checking){
-            book.users = book.users.filter(user => user.id !== 1)
-            updateBook(book)
-            refresh(book)       
-        }})
+        unlike.addEventListener('click', () => {
+            const checking = book.users.find(user => user.id == 1)
+            if (checking) {
+                book.users = book.users.filter(user => user.id !== 1)
+                const user_li = div.querySelector(`li[data-id='${currentUser.id}']`)
+                user_li.remove()
+                updateBook(book)
+
+            }
+        })
     })
 }
 
-
 const refresh = book => {
+    book.users = book.users.filter(user => user.id !== 1)
+    const usershtml = book.users.map(user => `<li>${user.username}</li>`).join('')
     div.innerHTML = `
         <h1>${book.title}</h1>
         <img src="${book.img_url}" alt="">
         <p>${book.description}</p>
         <button id=read >Read Book</button>
         <button id=unlike >UnLike Book</button>
-        <h3>People that have read this book:</h3>`
+        <h3>People that have read this book:</h3>
+        <ul class'readers'>${usershtml}</ul>`
+}
 
-    book.users.forEach(user => {
-        div.innerHTML += `<h3>${user.username}</h3>`
-    })
+const checkUser = book => {
+    const newLi = document.createElement('li')
+    newLi.dataset.id = currentUser.id
+    const ul = div.querySelector('ul')
+    book.users.push(currentUser)
+    updateBook(book)
+    newLi.innerText = currentUser.username
+    ul.append(newLi)
 }
 
 const renderBooks = books => {
